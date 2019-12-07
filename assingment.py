5f4b4752a1877d7249d4597ca873a530369eae6f
AndrewTHowell <howelldrew99@gmail.com>
2019-12-07 23:19:46 +0000

    Disparity formatted, ready for yolo integration

--- assingment.py
+++ assingment.py
@@ -6,7 +6,7 @@
 # License : LGPL - http://www.gnu.org/licenses/lgpl.html
 #####################################################################
 
-# Section: Import Modules
+# Section: Import modules
 
 import cv2
 import os
@@ -14,135 +14,161 @@ import numpy as np
 
 # Section End
 
-# Section: Set Path to the Video Dataset
+# Section: Set path to the video dataset
 
 master_path_to_dataset = "D:/howel/Videos/Computer Vision Coursework"
 
 # Section End
 
-DEBUG = False
+# Section: File handling
 
-directory_to_cycle_left = "left-images";     # edit this if needed
-directory_to_cycle_right = "right-images";   # edit this if needed
-
-# set this to a file timestamp to start from (empty is first example - outside lab)
-# e.g. set to 1506943191.487683 for the end of the Bailey, just as the vehicle turns
-skip_forward_file_pattern = ""; # set to timestamp to skip forward to
-
-crop_disparity = False; # display full or cropped disparity image
-pause_playback = False; # pause until key press after each image
-
-#####################################################################
+leftImagesPath = "left-images"     # edit this if needed
+rightImagesPath = "right-images"   # edit this if needed
 
 # resolve full directory location of data set for left / right images
-full_path_directory_left =  os.path.join(master_path_to_dataset, directory_to_cycle_left);
-full_path_directory_right =  os.path.join(master_path_to_dataset, directory_to_cycle_right);
+fullPathLeftImages = os.path.join(master_path_to_dataset, leftImagesPath)
+fullPathRightImages = os.path.join(master_path_to_dataset, rightImagesPath)
 
 # get a list of the left image files and sort them (by timestamp in filename)
-left_file_list = sorted(os.listdir(full_path_directory_left));
+imageNameListL = sorted(os.listdir(fullPathLeftImages))
+
+# Section End
+
+# Section: Create stereoProcessor object
 
 # setup the disparity stereo processor to find a maximum of 128 disparity values
 # (adjust parameters if needed - this will effect speed to processing)
-
-# uses a modified H. Hirschmuller algorithm [Hirschmuller, 2008] that differs (see opencv manual)
-# parameters can be adjusted, current ones from [Hamilton / Breckon et al. 2013]
-
 #    StereoSGBM_create(...)
 #        StereoSGBM_create(minDisparity, numDisparities, blockSize[, P1[, P2[,
 # disp12MaxDiff[, preFilterCap[, uniquenessRatio[, speckleWindowSize[, speckleRange[, mode]]]]]]]]) -> retval
+maxDisparity = 128
+stereoProcessor = cv2.StereoSGBM_create(0, maxDisparity, 21)
 
-max_disparity = 128;
-stereoProcessor = cv2.StereoSGBM_create(0, max_disparity, 21);
+# Section End
 
-for filename_left in left_file_list:
+# Region: User input variables
 
-    # skip forward to start a file we specify by timestamp (if this is set)
-    if ((len(skip_forward_file_pattern) > 0) and not(skip_forward_file_pattern in filename_left)):
-        continue;
-    elif ((len(skip_forward_file_pattern) > 0) and (skip_forward_file_pattern in filename_left)):
-        skip_forward_file_pattern = "";
+cropDisparity = False  # display full or cropped disparity image
+pausePlayback = False  # pause until key press after each image
 
-    # from the left image filename get the correspondoning right image
-    filename_right = filename_left.replace("_L", "_R");
-    full_path_filename_left = os.path.join(full_path_directory_left, filename_left);
-    full_path_filename_right = os.path.join(full_path_directory_right, filename_right);
+# set this to a file timestamp to start from (empty is first example - outside lab)
+# e.g. set to 1506943191.487683 for the end of the Bailey, just as the vehicle turns
+startTimestamp = ""  # set to timestamp to skip forward to
 
-    # for sanity print out these filenames
-    if DEBUG:
-        print()
-        print(full_path_filename_left);
-        print(full_path_filename_right);
+# Region End
 
-    # check the file is a PNG file (left) and check a correspondoning right image
-    # actually exists
-    if ('.png' in filename_left) and (os.path.isfile(full_path_filename_right)):
+# Section: Iteration through image files
 
-        # read left and right images and display in windows
-        # N.B. despite one being grayscale both are in fact stored as 3-channel
-        # RGB images so load both as such
-        imgL = cv2.imread(full_path_filename_left, cv2.IMREAD_COLOR)
-        cv2.imshow('left image',imgL)
-        imgR = cv2.imread(full_path_filename_right, cv2.IMREAD_COLOR)
-        cv2.imshow('right image',imgR)
-        print("-- files loaded successfully");
+for imageNameL in imageNameListL:
 
-        # remember to convert to grayscale (as the disparity matching works on grayscale)
-        # N.B. need to do for both as both are 3-channel images
-        grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY);
-        grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY);
+    # Region: Skip to specific timestamp
 
-        # perform preprocessing - raise to the power, as this subjectively appears
-        # to improve subsequent disparity calculation
-        grayL = np.power(grayL, 0.75).astype('uint8');
-        grayR = np.power(grayR, 0.75).astype('uint8');
+    # Skip to timestamp file (if this is set)
+    if ((len(startTimestamp) > 0) and not(startTimestamp in imageNameL)):
+        continue
+    elif ((len(startTimestamp) > 0) and (startTimestamp in imageNameL)):
+        startTimestamp = ""
 
-        # compute disparity image from undistorted and rectified stereo images
-        # that we have loaded
-        # (which for reasons best known to the OpenCV developers is returned scaled by 16)
-        disparity = stereoProcessor.compute(grayL,grayR);
+    # Region End
 
-        # filter out noise and speckles (adjust parameters as needed)
-        dispNoiseFilter = 5; # increase for more agressive filtering
-        cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
+    # Derive path for right image from left image
+    imageNameR = imageNameL.replace("_L", "_R")
+    fullPathLeftImage = os.path.join(fullPathLeftImages, imageNameL)
+    fullPathRightImage = os.path.join(fullPathRightImages, imageNameR)
 
-        # scale the disparity to 8-bit for viewing
-        # divide by 16 and convert to 8-bit image (then range of values should
-        # be 0 -> max_disparity) but in fact is (-1 -> max_disparity - 1)
-        # so we fix this also using a initial threshold between 0 and max_disparity
-        # as disparity=-1 means no disparity available
-        _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
-        disparity_scaled = (disparity / 16.).astype(np.uint8);
+    # check the file is a PNG file (left) and check a correspondoning right
+    # image actually exists
+    if ('.png' in imageNameL) and (os.path.isfile(fullPathRightImage)):
 
-        # crop disparity to chop out left part where there are with no disparity
-        # as this area is not seen by both cameras and also
-        # chop out the bottom area (where we see the front of car bonnet)
-        if (crop_disparity):
-            width = np.size(disparity_scaled, 1);
-            disparity_scaled = disparity_scaled[0:390,135:width];
+        # Region: Display left and right images
+
+        # Read left and right images and display in windows
+        imgL = cv2.imread(fullPathLeftImage, cv2.IMREAD_COLOR)
+        cv2.imshow('left image', imgL)
+        imgR = cv2.imread(fullPathRightImage, cv2.IMREAD_COLOR)
+        cv2.imshow('right image', imgR)
+
+        # Region End
+
+        # Convert to grayscale (as the disparity matching works on grayscale)
+        grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+        grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
+
+        # Region: PREPROCESSING
+
+        # Raise to the power, appears to improve subsequent disparity calculation
+        grayL = np.power(grayL, 0.75).astype('uint8')
+        grayR = np.power(grayR, 0.75).astype('uint8')
+
+        # Region End
+
+        # Compute disparity image (returned scaled by 16)
+        disparity = stereoProcessor.compute(grayL, grayR)
+
+        # Region: POSTPROCESSING
+
+        # Filter out noise and speckles (adjust parameters as needed)
+        dispNoiseFilter = 5  # increase for more agressive filtering
+        cv2.filterSpeckles(disparity, 0, 4000, maxDisparity - dispNoiseFilter)
+
+        # Region End
+
+        # Region: Prep for/Display Disparity Image
+
+        # Scale the disparity to 8-bit for viewing
+        _, disparity = cv2.threshold(disparity, 0, maxDisparity * 16, cv2.THRESH_TOZERO)
+        disparityScaled = (disparity / 16.).astype(np.uint8)
+
+        # If cropping wanted:
+        if (cropDisparity):
+            # Crop left part of disparity image where not seen by both cameras
+            width = np.size(disparityScaled, 1)
+            # Crop out the car bonnet
+            disparityScaled = disparityScaled[0:390, 135:width]
 
         # display image (scaling it to the full 0->255 range based on the number
         # of disparities in use for the stereo part)
-        cv2.imshow("disparity", (disparity_scaled * (256. / max_disparity)).astype(np.uint8));
+        cv2.imshow("disparity", (disparityScaled * (256. / maxDisparity)).astype(np.uint8))
 
-        # keyboard input for exit (as standard), save disparity and cropping
+        # Region End
+
+        nearestObjectDistance = -1
+
+        # Output filenames and nearest detected scene object
+        print("{0}.png\n{1}.png : nearest detected scene object ({2:.1}m)"
+              .format(imageNameL, imageNameR, nearestObjectDistance))
+
         # exit - x
         # save - s
         # crop - c
         # pause - space
-        key = cv2.waitKey(40 * (not(pause_playback))) & 0xFF; # wait 40ms (i.e. 1000ms / 25 fps = 40 ms)
+        # Region: User Input
+
+        desiredFPS = 25
+        keyDelay = 1000 / desiredFPS  # e.g: 1000ms / 25 fps = 40 ms)
+        key = cv2.waitKey(40 * (not(pausePlayback))) & 0xFF
+        # keyboard input for exit (as standard), save disparity and cropping
         if (key == ord('x')):       # exit
-            break; # exit
+            break  # exit
+
         elif (key == ord('s')):     # save
-            cv2.imwrite("sgbm-disparty.png", disparity_scaled);
-            cv2.imwrite("left.png", imgL);
-            cv2.imwrite("right.png", imgR);
+            cv2.imwrite("sgbm-disparty.png", disparityScaled)
+            cv2.imwrite("left.png", imgL)
+            cv2.imwrite("right.png", imgR)
+
         elif (key == ord('c')):     # crop
-            crop_disparity = not(crop_disparity);
+            cropDisparity = not(cropDisparity)
+
         elif (key == ord(' ')):     # pause (on next frame)
-            pause_playback = not(pause_playback);
+            pausePlayback = not(pausePlayback)
+
+        # Region End
+
     else:
-            print("-- files skipped (perhaps one is missing or not PNG)");
-            print();
+        print("-- files skipped (perhaps one is missing or not PNG)")
+        print()
+
+# Section End
 
 # close all windows
 cv2.destroyAllWindows()
